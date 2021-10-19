adff0971e11cd839c07e672edaac8c2f96053786
5iveemmy <ainemmanuel87@gmail.com>
2021-10-19 14:03:33 +0100

    acvaj

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useState } from "react";
 import { SliderData } from "./SliderData";
 import { TiMediaPlay, TiMediaPlayReverse } from "react-icons/all";
 import "../styles/ImageSlider.css";
