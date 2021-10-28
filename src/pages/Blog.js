7b493a42e95a0626e94b1daa2077ff9254edb1e7
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2021-10-28 12:50:19 +0100

    blog text added

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,13 +1,16 @@
 import React from "react";
 import blog from "../images/bg-02.jpg";
+import "../styles/Pages.css";
 
 function Blog() {
   return (
     <div className="blog">
       <div className="blog-image">
         <img src={blog} alt="blog" />
+        <div className="blog-text-wrapper">
+          <h1 className="blog-text">Blog</h1>
+        </div>
       </div>
-      <div className="blog-wrappper"></div>
     </div>
   );
 }
