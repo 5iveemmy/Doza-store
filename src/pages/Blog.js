213237064a26c5924c311175e3b2aa311e2f4dea
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2021-10-28 15:33:10 +0100

    seson sect created

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,6 +1,7 @@
 import React from "react";
 import blog from "../images/bg-02.jpg";
 import "../styles/Pages.css";
+import blogOne from "../images/blog-01.jpg";
 
 function Blog() {
   return (
@@ -11,6 +12,19 @@ function Blog() {
           <h1 className="blog-text">Blog</h1>
         </div>
       </div>
+      <section className="blog-sect">
+        <div className="blog-sect-wrapper">
+          <div className="season">
+            <div className="season-wrap">
+              <div className="winter">
+                <div className="winter-image">
+                  <img src={blogOne} alt="blog01" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
