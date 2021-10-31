1cc4ca8bf0f924fd35818ed006c75b1a72f28b13
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2021-10-31 05:07:23 +0100

    enlarge on hover now works

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,5 +1,6 @@
 import React from "react";
 import blog from "../images/bg-02.jpg";
+import { Link } from "react-router-dom";
 import "../styles/Pages.css";
 import blogOne from "../images/blog-04.jpg";
 
@@ -14,11 +15,42 @@ function Blog() {
       </div>
       <section className="blog-sect">
         <div className="blog-sect-wrapper">
-          <div className="season">
-            <div className="season-wrap">
-              <div className="winter">
+          <div className="for-winter">
+            <div className="winter">
+              <Link to="/">
                 <div className="winter-image">
                   <img src={blogOne} alt="blog01" className="blog-one-image" />
+                </div>
+                <div className="season-date">
+                  <span className="num-span">01</span>
+                  <span className="month-span">Oct 2021</span>
+                </div>
+              </Link>
+              <div className="winter-text">
+                <h4>
+                  {" "}
+                  <Link> 8 Inspiring Ways to Wear Dresses in the winter</Link>
+                </h4>
+                <p>
+                  Class aptent taciti sociosqu ad litora torquent per conubia
+                  nostra, per inceptos himenaeos, Fusce eget dictum tortot.
+                  Donec dictum vitae sapien eu varius
+                </p>
+                <div className="winter-tiny">
+                  <span>
+                    <span>
+                      <span>
+                        <span>By</span>Admin
+                      </span>
+                      <span>|</span>
+                    </span>
+                    <span>
+                      StreetStyle,Fashion, Couple
+                      <span>|</span>
+                    </span>
+                    <span>8 Comments</span>
+                  </span>
+                  <Link>CONTINUE READING</Link>
                 </div>
               </div>
             </div>
